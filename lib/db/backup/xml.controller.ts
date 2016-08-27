0b0ea079c9d5d092d7f435705a77d3c224617ed3
Noam <noam@mail.com>
2016-08-27 13:19:57 +0300

    backup to xml router

--- lib/db/backup/xml.controller.ts
+++ lib/db/backup/xml.controller.ts
@@ -9,7 +9,10 @@ export class XmlController {
     constructor(private _xmlService: XmlService) { }
 
     public backupToXml(req: e.Request, res: e.Response, next: Function) {
-
+        this._xmlService.backupToXml()
+            .then((xmlData) => {
+                res.send(xmlData);
+            });
     }
 
     public backupFromXml(req: e.Request, res: e.Response, next: Function) {
